d4edadfdd80d333be5bdf89bd30d6f210682f1fa
Vidyapati <vidyapati@microvac.co.id>
2017-10-26 14:07:02 +0700

    Update migrations use snake case

--- ReformaAgraria/Models/ReformaAgrariaDbContext.cs
+++ ReformaAgraria/Models/ReformaAgrariaDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using MicrovacWebCore;
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -51,6 +52,33 @@ namespace ReformaAgraria.Models
                 .HasForeignKey(e => e.UserId)
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Cascade);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                // Replace table names
+                entity.Relational().TableName = entity.Relational().TableName.ToSnakeCase();
+
+                // Replace column names            
+                foreach (var property in entity.GetProperties())
+                {
+                    property.Relational().ColumnName = property.Name.ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.Relational().Name = index.Relational().Name.ToSnakeCase();
+                }
+            }
         }
 
         public override int SaveChanges()
@@ -96,6 +124,17 @@ namespace ReformaAgraria.Models
                 ((IAuditableEntity)entity.Entity).DateModified = DateTime.UtcNow;
                 //((IAuditableEntity)entity.Entity).UserModified = currentUsername;
             }
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) { return input; }
+
+            var startUnderscores = Regex.Match(input, @"^_+");
+            return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
         }
     }
 }
