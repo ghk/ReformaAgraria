26bd7465b855b6c9d66568a29da51d21e72fe0a3
Barry Darmawan <waverome@gmail.com>
2017-11-14 14:11:45 +0700

    Update UI Objek subjek and account

--- ReformaAgraria/ClientApp/app/components/agraria-issues-list.ts
+++ ReformaAgraria/ClientApp/app/components/agraria-issues-list.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'ra-agraria-issues-list',
@@ -9,10 +10,21 @@ export class AgrariaIssuesListComponent implements OnInit, OnDestroy {
     constructor() { }
 
     ngOnInit(): void {
-
     }
 
     ngOnDestroy(): void {
+
+    }
+
+    onToggle(id){
+        let img = $("#"+id+" img");
+        if(img.hasClass("spin-icon")){
+            img.removeClass("spin-icon");
+            img.addClass("back-spin");
+        } else {
+            img.removeClass("back-spin");
+            img.addClass("spin-icon");
+        }
 
     }
 
