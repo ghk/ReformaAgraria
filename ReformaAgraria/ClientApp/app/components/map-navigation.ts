c1e08728d64bcaa38329d1ef3cee537610e0362a
Barry Darmawan <waverome@gmail.com>
2017-12-29 14:24:43 +0700

    update ui

---
+++ ReformaAgraria/ClientApp/app/components/map-navigation.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'ra-map-navigation',
+    templateUrl: '../templates/map-navigation.html',
+})
+export class MapNavigationComponent implements OnInit, OnDestroy {
+
+    constructor() { }
+
+    ngOnInit(): void {
+
+    }
+
+    ngOnDestroy(): void {
+
+    }
+
+}
---
+++ ReformaAgraria/Models/VillageMapAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReformaAgraria.Models
+{
+    public class VillageMapAttribute: BaseEntity<int>
+    {
+        public VillageMapAttribute() { }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override int Id { get; set; }        
+
+        public decimal Size { get; set; }
+
+        public BorderSettingProcessStage BorderSettingProcessStage { get; set; }
+
+        public Status BorderSettingStatus { get; set; }
+
+        public string Attachment { get; set; }
+        
+        public int? FkCoordinateId { get; set; }
+
+        [ForeignKey("FkCoordinateId")]
+        public Coordinate Coordinate { get; set; }
+
+        public string FkRegionId { get; set; }
+
+        [ForeignKey("FkRegionId")]
+        public Region Region { get; set; }
+
+    }
+}
