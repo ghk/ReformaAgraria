27301df28bbeb7e35f7de898a8ea64b6daf72589
root <root@debian>
2018-01-15 16:45:18 +0700

    Edit build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sudo git pull
 sudo systemctl stop kestrel-reforma.service
-sudo dotnet publish -c Release
+sudo dotnet publish -c Release -r debian.8-x64
 sudo systemctl start kestrel-reforma.service
