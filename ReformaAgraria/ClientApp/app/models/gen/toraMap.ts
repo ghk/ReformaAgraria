64ae38f5b8c65a6d95ef01e462c578edd1242c12
eandanto <erlangga.andanto@gmail.com>
2018-01-29 10:43:36 +0700

    add translation

--- ReformaAgraria/ClientApp/app/models/gen/toraMap.ts
+++ ReformaAgraria/ClientApp/app/models/gen/toraMap.ts
@@ -6,6 +6,7 @@ export interface ToraMap extends BaseEntity<number> {
     id?: number;
     name?: string;
     geojson?: string;
+    size?: number;
     fkToraObjectId?: number;
     fkRegionId?: string;
     toraObject?: ToraObject;
